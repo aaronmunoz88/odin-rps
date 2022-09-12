1ed2291b0505de93a29743b8a51c349be3af7091
Aaron Munoz <aaronmunoz88@gmail.com>
2022-09-12 09:03:12 -0700

    Add score keeping

--- java.js
+++ java.js
@@ -1,27 +1,34 @@
 
-
+  let playerScore = 0
+  let computerScore = 0
 
 function playRound() {
   const gameOptions = ["rock", "paper", "scissors"];
   const computerChoice = Math.floor(Math.random() * gameOptions.length)
   let computer = gameOptions[computerChoice]
-  let playerChoice = prompt("Rock, Paper, or Scissors?")
+  let playerChoice = prompt("Best out of 5! Rock, Paper, or Scissors?")
   let player1 = playerChoice.toLowerCase()
 
   if (player1 === computer) {
     console.log("Tie")
   } else if (player1 === "rock" && computer === "paper") {
-    console.log("Paper beats your bitch ass Rock, you lose!!!")
+    computerScore++;
+    console.log(`Paper beats your bitch ass Rock, you lose!!! Score: Player:${playerScore}, Computer:${computerScore}` )
   } else if (player1 === "rock" && computer === "scissors") {
-    console.log("Rock beats Scissors! You crushed it!")
+    playerScore++;
+    console.log(`Rock beats Scissors! You crushed it! Score: Player:${playerScore}, Computer:${computerScore}`)
   } else if (player1 === "paper" && computer === "rock") {
-    console.log("Paper beats Rock! Wrap it up!!")
+    playerScore++;
+    console.log(`Paper beats Rock! Wrap it up!! Score: Player:${playerScore}, Computer:${computerScore}`)
   } else if (player1 === "paper" && computer === "scissors") {
-    console.log("Scissors cuts Paper! You lose big time!!!")
+    computerScore++;
+    console.log(`Scissors cuts Paper! You lose big time!!! Score: Player:${playerScore}, Computer:${computerScore}`)
   } else if (player1 === "scissors" && computer === "rock") {
-    console.log("Rock crushes your Scissors! Ouch!")
+    computerScore++;
+    console.log(`Rock crushes your Scissors! Ouch! Score: Player:${playerScore}, Computer:${computerScore}`)
   } else if (player1 === "scissors" && computer === "paper") {
-    console.log("Scissors cuts Paper! It's super effective!")
+    playerScore++;
+    console.log(`Scissors cuts Paper! It's super effective! Score: Player:${playerScore}, Computer:${computerScore}`)
   }
 }
 
@@ -29,7 +36,7 @@ function playRound() {
 function game() {
   for (let i = 0; i <= 5; i++) {
     if (i === 5) {
-      console.log("Game Over");
+      console.log(`Game Over. Final score: Player:${playerScore}, Computer:${computerScore}`);
     } else {
       playRound()
     }
@@ -38,27 +45,3 @@ function game() {
 
 
 game()
-
-
-
-
-
-
-/*function playRound() {
-  if (player1 === computer) {
-    console.log("Tie")
-  } else if (player1 === "rock" && computer === "paper") {
-    console.log("Paper beats your bitch ass Rock, you lose!!!")
-  } else if (player1 === "rock" && computer === "scissors") {
-    console.log("Rock beats Scissors! You crushed it!")
-  } else if (player1 === "paper" && computer === "rock") {
-    console.log("Paper beats Rock! Wrap it up!!")
-  } else if (player1 === "paper" && computer === "scissors") {
-    console.log("Scissors cuts Paper! You lose big time!!!")
-  } else if (player1 === "scissors" && computer === "rock") {
-    console.log("Rock crushes your Scissors! Ouch!")
-  } else if (player1 === "scissors" && computer === "paper") {
-    console.log("Scissors cuts Paper! It's super effective!")
-  }
-}
-*/
